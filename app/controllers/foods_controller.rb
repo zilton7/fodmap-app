9e6af90fda31f163cd11a3c862df277406361687
Zil Norvilis <zilasino27@gmail.com>
2026-03-31 21:39:03 +0300

    Add create/edit food functionality

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,4 +1,6 @@
 class FoodsController < ApplicationController
+  before_action :set_food, only: %i[ show edit update destroy ]
+
   def index
     @foods = Food.all
     if params[:query].present?
@@ -14,6 +16,51 @@ class FoodsController < ApplicationController
   end
 
   def show
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def edit
+  end
+
+  def create
+    @food = Food.new(food_params)
+
+    if @food.save
+      redirect_to @food, notice: "Food was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @food.update(food_params)
+      redirect_to @food, notice: "Food was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @food.destroy
+    redirect_to foods_url, notice: "Food was successfully destroyed."
+  end
+
+  private
+
+  def set_food
     @food = Food.find(params[:id])
+  end
+
+  def food_params
+    params.require(:food).permit(
+      :name, :category, :overall_status, :image_file_name, :notes,
+      :small_portion_desc, :small_portion_fructose, :small_portion_lactose,
+      :small_portion_oligos, :small_portion_polyols,
+      :large_portion_desc, :large_portion_fructose, :large_portion_lactose,
+      :large_portion_oligos, :large_portion_polyols
+    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
-  resources :foods, only: [ :index, :show ]
+  resources :foods
 
   root "foods#index"
 end
